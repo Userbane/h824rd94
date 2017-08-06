2078579383e6cf5761444f748add304dbb7eface
userbane <zdeanmiloz@outlook.com>
2017-08-06 15:09:07 +0100

    Good progress on title card: ----Card now changes form depending on the pageYOffset. ----Halfway done on adding the contents to the expanded title-card.

--- portfolio/web/script.js
+++ portfolio/web/script.js
@@ -1,31 +1,36 @@
-window.addEventListener('scroll', debounce(checkScroll()));
-function debounce(func, wait = 20, immediate = true) {
-      var timeout;
-      return function() {
-        var context = this, args = arguments;
-        var later = function() {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-      };
-    };
-    
+
+/*
+window.onload = function(){
+    //altering elements based on the yOffset of the page.
+    window.addEventListener('scroll', debounce(checkScroll));
+  
+    var nav = document.getElementsByTagName('nav')[0];
+
+
     function checkScroll(){
-        if( y == 0 ){
-            nav.style.height = 100 + 'vh';
+        var y = pageYOffset;
+        if(y <= 15){
+            nav.style.height = 100+'vh';
+        }else if( y > 15){
+            nav.style.height = 10+'vh';
         }
     }
+    function debounce(func, wait = 10, immediate = true) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+    
+
+}*/
+
  
-window.onload = () => {
-    var nav = document.getElementsByTagName('nav')[0];
-    var  y = pageYOffset;
-    
-    
-    
-    
-    
-}
